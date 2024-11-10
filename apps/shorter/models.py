573475ceeb8e2e9666ffae4ba583dedf468d427d
faridhaavasi <faridhavasi777@gmail.com>
2024-11-10 16:56:02 +0330

    model link

--- apps/shorter/models.py
+++ apps/shorter/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import string, random
 
-# Create your models here.
+class Link(models.Model):
+    original_url = models.URLField(max_length=500)
+    short_code = models.CharField(max_length=10, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = self.generate_short_code()
+        super().save(*args, **kwargs)
+
+    def generate_short_code(self):
+        length = 6  
+        while True:
+            code = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+            if not Link.objects.filter(short_code=code).exists():
+                return code
+
+    def __str__(self):
+        return f"{self.original_url} -> {self.short_code}"
