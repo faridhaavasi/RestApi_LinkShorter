6f8dd54852acf030c49002a0f4df0b890d9e98ec
faridhaavasi <faridhavasi777@gmail.com>
2024-11-16 14:29:30 +0330

    passed one test

--- apps/accounts/tests/test_account.py
+++ apps/accounts/tests/test_account.py
@@ -1,6 +1,24 @@
 from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@pytest.fixture
+def common_user():
+    return User.objects.create_user(
+        email='example@gmail.com',
+        password='1123',
+        is_verify=True
+    )
+
 
 class TestAccounts:
-    pass
+    client = APIClient()
+
+    @pytest.mark.django_db
+    def test_get_ProfileView_status_200(self, common_user):
+        self.client.force_authenticate(user=common_user)
+        response = self.client.get(reverse('accounts:profile-view'))  
+        assert response.status_code == 200
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 
-app_name = 'account'
 urlpatterns = [
     path('api/v1/', include('apps.accounts.v1.urls')),
     
