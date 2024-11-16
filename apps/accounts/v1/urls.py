19ead0d9bd3697ffe2cc55364576e727879dd5af
faridhaavasi <faridhavasi777@gmail.com>
2024-11-16 14:04:17 +0330

    add app name

--- apps/accounts/v1/urls.py
+++ apps/accounts/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from apps.accounts.v1.views.profiile import ProfileView, EditProfileView
+app_name = 'accounts'
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile-view'), 
     path('profile/edit/', EditProfileView.as_view(), name='edit-profile'),  
