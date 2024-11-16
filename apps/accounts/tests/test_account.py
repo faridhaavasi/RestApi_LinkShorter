3b29c01e76e54a4059cc23fd06d384c8306659f2
faridhaavasi <faridhavasi777@gmail.com>
2024-11-16 18:17:51 +0330

    tst accunt passed

--- apps/accounts/tests/test_account.py
+++ apps/accounts/tests/test_account.py
@@ -2,6 +2,7 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
 from django.contrib.auth import get_user_model
+from apps.accounts.models import Account
 
 User = get_user_model()
 
@@ -12,6 +13,11 @@ def common_user():
         password='1123',
         is_verify=True
     )
+@pytest.fixture
+def create_account(common_user):
+    account, created = Account.objects.get_or_create(user=common_user)
+    return account
+
 
 
 class TestAccounts:
@@ -20,5 +26,16 @@ class TestAccounts:
     @pytest.mark.django_db
     def test_get_ProfileView_status_200(self, common_user):
         self.client.force_authenticate(user=common_user)
-        response = self.client.get(reverse('accounts:profile-view'))  
+        response = self.client.get(reverse('accounts:profile-view'))
         assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test_EditProfileView_with_valid_data(self, common_user, create_account):
+        data = {
+            'bio': 'test',
+            'id_user': 'test'
+        }
+        self.client.force_authenticate(user=common_user)
+
+        response = self.client.put(reverse('accounts:edit-profile'), data=data)
+        assert response.status_code == 200  
