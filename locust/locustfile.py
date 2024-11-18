cb4b3ad3fe914ca5aa083e7a74a00f7c1891152a
faridhaavasi <faridhavasi777@gmail.com>
2024-11-18 16:23:17 +0330

    start work in locust framwork

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,15 +1,5 @@
+
 from locust import HttpUser, task, between
 
-class WebsiteUser(HttpUser):
+class QuickstartUser(HttpUser):
     pass
-    # wait_time = between(1, 5)
-
-    # @task
-    # def load_homepage(self):
-    #     self.client.get("/")
-    
-    # @task
-    # def create_short_link(self):
-    #     self.client.post("/api/v1/short-links/", json={
-    #         "original_url": "https://example.com"
-    #     })
